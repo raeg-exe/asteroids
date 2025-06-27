32ebc2e31499339b5033d47843e251b0d9280e43
raeg-exe <raeg.exe@aol.com>
2025-06-27 15:32:50 -0400

    set up for asteroids

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+        main()
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("black")
+
+    pygame.display.flip()
