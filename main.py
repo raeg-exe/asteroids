f19fc750d4b7ad16624af33b1c61d8c111379a50
raeg-exe <raeg.exe@aol.com>
2025-06-28 15:50:38 -0400

    drawing the player

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     print("Starting Asteroids!")
@@ -12,13 +14,19 @@ if __name__ == "__main__":
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
+dt = 0
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
     screen.fill("black")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player.draw(screen)
 
     pygame.display.flip()
